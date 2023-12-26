b16d1866b18c648c8e2bebc6c35fcaab3dac52e5
shahin hossain <shahinhossain3@hotmail.com>
2023-12-26 22:19:50 +0600

    react project setup with react router dom, tailwind CSS, Daisy UI

---
+++ __Steps__.js
@@ -0,0 +1,12 @@
+/**
+ * React Router - React Vite Project
+ * tailwind CSS
+ * Daisy UI
+ * 
+ * set Basic Routing with react router dom
+ * component make করার আগে দেখতে হবে। 
+ * project কেমন component লাগতে পারে সে অনুযায়ী folder structure করতে হবে।
+ * project start করার আগে কি কি করা লাগবে কিভাবে করলে ভালো হবে সে বিষয়ে আগে  একটু চিন্তা করে নিতে হবে।
+ * 
+ * 
+ */
